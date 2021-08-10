5bd218ad927f00ec04f3b69d6c909b79bd8967f0
Sanatan Kafle <kaflesanatan100@gmail.com>
2021-08-10 10:42:40 +0545

    Added computer move in tic tac toe

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,6 +1,7 @@
+import random
 
 grid=['0','1','2','3','4','5','6','7','8']
-
+number_list = [0,1,2,3,4,5,6,7,8]
 
 def draw_board():
     print(grid[0], "|" , grid[1], "|" ,grid[2])
@@ -8,7 +9,6 @@ def draw_board():
     print(grid[3], "|" , grid[4], "|" ,grid[5])
     print("__  __  __")
     print(grid[6], "|" , grid[7], "|" ,grid[8])
-
 
 def check_win():
     if grid[0]==grid[1]==grid[2] =="X" or grid[3]==grid[4]==[5] =="X" or grid[6]==grid[7]==grid[8]=="X" or grid[0]==grid[4]==grid[8]=="X" or grid[2]==grid[4]==grid[6] =="X" or grid[0]==grid[3]==grid[6] =="X" or grid[1]==grid[4]==grid[7] =="X" or grid[2]==grid[5]==grid[8]=="X" :
@@ -28,24 +28,47 @@ def check_valid(number):
         return 1
 
 
+def computer_move():
+    computer_choice = random.choice(number_list)
+    return computer_choice
+
+
 def main():
-    print("Play with X as player 1 and O with player 2. \n")
+    versus = input("\n\t\t\tEnter 1 to play with computer and 2 to play one on one..\n\n")
+    try:
+        if versus == '1':
+            print("Play second with computer. ")
+        if versus == '2':
+            print("Play one on one. ")
+    except:
+        print("Invalid input....")
     n=1
     draw_board()
     while n<10:
         if n%2!=0:
-            input_1 = int(input("Player1 turn.\nEnter Number you want to put X in: "))
-            if check_valid(input_1) == 1:
-                grid[input_1] = 'X'
-            else:
-                print("Invalid Space.. use other space")
-                draw_board()
-                continue
+            if versus == '1':
+                print("Computer's turn")
+                input_1 = computer_move()
+                if check_valid(input_1) == 1:
+                    grid[input_1] = 'X'
+                    number_list.remove(input_1)
+                else:
+                    print("Invalid Space.. use other space")
+                    draw_board()
+                    continue
+            elif versus == '2':
+                input_1 = int(input("Player1 turn.\nEnter Number you want to put X in: "))
+                if check_valid(input_1) == 1:
+                    grid[input_1] = 'X'
+                else:
+                    print("Invalid Space.. use other space")
+                    draw_board()
+                    continue
         else:
             input_2 = int(input("Player2 turn.\nEnter Number you want to put O in: "))
             if check_valid(input_2) == 1:
                 grid[input_2] = "O"
-            
+                number_list.remove(input_2)
             else:
                 print("Invalid Space.. use other space")
                 draw_board()
@@ -53,7 +76,7 @@ def main():
         n+=1
 
         if check_win()==0:
-            print("Congrats player 1 won!!!")
+            print("You Lost!!!")
             draw_board()
             break
         if check_win()==1:
